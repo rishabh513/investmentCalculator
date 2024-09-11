970777540c237397ba293715a68bb94ddff40898
rishabh513 <rishabhtava52@gmail.com>
2024-09-11 10:05:53 -0400

    adding the hedaer component for the application

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/investment-calculator.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Logo shwoing the inevstment calculator" />
+      <h1>Investment calculator</h1>
+    </header>
+  );
+}
